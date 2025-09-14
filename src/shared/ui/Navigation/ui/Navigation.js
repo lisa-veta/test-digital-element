f97f1df03dd19ff29c59233a6607f1f1b83b96af
Elizaveta Pernikova <lisapernikowa@mail.ru>
2025-09-14 18:15:36 +0500

    feat: add Navigation

---
+++ src/shared/ui/Navigation/ui/Navigation.js
@@ -0,0 +1,17 @@
+import { LogoIcon, MenuIcon } from "@shared/ui/Icons/index.js";
+import "../style.scss";
+
+/**
+ * Рендерит компонент навигации с логотипом и иконкой меню
+ *
+ * @returns {string} HTML-разметка навигации
+ */
+export const Navigation = () => `
+<div class="navigation">
+    <div class="navigation__logo">
+      ${LogoIcon()}
+    </div>
+    <div class="navigation__menu">
+        ${MenuIcon()}
+    </div>
+</div>`;
