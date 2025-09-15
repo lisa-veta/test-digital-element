6816d8eb69edfde13b6d93d8baf4bb0067f60cf3
Elizaveta Pernikova <lisapernikowa@mail.ru>
2025-09-15 13:34:26 +0500

    feat: add Features

---
+++ src/pages/home/Features/constants.js
@@ -0,0 +1,17 @@
+export const INFO_CARD_1 = {
+    image: "illustration1",
+    title: "Make Your business To Be Better Famous In Internet",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+};
+
+export const INFO_CARD_2 = {
+    image: "illustration2",
+    title: "Bring Technology To Your Comfrotable Home",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+};
+
+export const INFO_CARD_3 = {
+    image: "illustration3",
+    title: "Build Your Digital Product That Suitable For Your Need",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+};
